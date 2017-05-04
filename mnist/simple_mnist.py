053567c49d64fb0135301a5f20620e7dc2617b45
Larry Neal <nealla@lwneal.com>
2017-05-03 21:54:29 -0700

    Update simple MNIST

--- mnist/simple_mnist.py
+++ mnist/simple_mnist.py
@@ -1,25 +1,20 @@
-# Imports
 import numpy as np
 from keras import layers, models, datasets
 
 # Keras provides this useful function for loading MNIST
 (x_train, y_train), (x_test, y_test) = datasets.mnist.load_data()
-assert x_train.shape == (60 * 1000, 28, 28)
 
-# This is a super-simple model: it's just a linear classifier!
-input_shape = (28, 28)
+# This is a super-simple model: a linear classifier with a softmax
 model = models.Sequential()
-model.add(layers.Flatten(input_shape=input_shape))
+model.add(layers.Flatten(input_shape=(28,28)))
 model.add(layers.Dense(10, activation='softmax'))
-model.compile(loss='sparse_categorical_crossentropy',
-        optimizer='adam', metrics=['accuracy'])
-# This is where we fit the model to the data
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam')
+
+# This performs gradient descent automatically
 model.fit(x_train, y_train, epochs=1)
 
-x = x_test[100:110]
-y_true = y_test[100:110]
-print("Ground Truth: {}".format(y_true))
-
-y = model.predict(x)
+# The model is now trained and can predict on new data
+y = model.predict(x_test[123:133])
 y = np.argmax(y, axis=-1)
+print("Ground Truth: {}".format(y_test[123:133]))
 print("Prediction:  {}".format(y))
